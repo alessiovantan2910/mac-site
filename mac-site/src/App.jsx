e326d2b2bb41770cde7ab2979dbbb68f8d3ddd77
kogachi <kogachi872.kdgo.student@gmail.com>
2026-01-31 10:01:50 +0900

    1/31 10:01

--- mac-site/src/App.jsx
+++ mac-site/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import NavBar from './components/NavBar.jsx'
+import Hero from './components/Hero.jsx'
+import ProductViewer from './components/ProductViewer.jsx'
+import gsap from 'gsap'
+import  { ScrollTrigger, } from 'gsap/all'
+import Showcase from './components/Showcase.jsx'
+import Performance from './components/Perfomance.jsx'
+import Features from './components/Features.jsx'
+import Highlights from './components/Highlights.jsx'
+import Footer from './components/Footer.jsx'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
-    <h1 className="flex flex-col items-center p-7 rounded-2xl">こんちゃ！</h1>
+    <main>
+        <NavBar/>
+        <Hero/>
+        <ProductViewer/>
+        <Showcase/>
+        <Performance/>
+        <Features/>
+        <Highlights/>
+        <Footer/>
+    </main>
   )
 }
 
